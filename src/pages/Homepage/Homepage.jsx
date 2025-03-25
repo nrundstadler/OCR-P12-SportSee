9640644e1197a431eefd45ea8a3b528016bb6f06
Nicolas Rundstadler <n.rundstadler@laposte.net>
2025-03-25 12:55:13 +0100

    chore: improve layout of the Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,15 @@
 function Homepage() {
   return (
-    <>
-      <header></header>
-    </>
+    <div>
+      <header>
+        <h1 className="mb-10 text-5xl font-medium">
+          Bonjour <span className="text-primary">Thomas</span>
+        </h1>
+        <p className="text-lg">
+          Félicitation ! Vous avez explosé vos objectifs hier 👏
+        </p>
+      </header>
+    </div>
   );
 }
 
